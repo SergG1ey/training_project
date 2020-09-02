4571903ed8030444d954be25a67da48ae0a8a430
SergG1ey <sergey.pilipenko1410@gmail.com>
2020-09-02 21:04:49 +0300

    lesson2

---
+++ lesson2/exc1.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    x = input("введите имя: ")
+    print("Hello, " + x + "!")
---
+++ lesson2/exc2.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    x = int(input("Please enter an integer number:"))
+    y = x + 1
+    z = x -1
+    print("The next number for the number %s is %s." % (x, y))
+    print("The previous number for the number %s is %s." % (x, z))
+
+
+
