ef1549a04ab6efbf51d6bb78b3a7f79d2b7993d7
SergG1ey <sergey.pilipenko1410@gmail.com>
2020-09-18 01:57:53 +0300

    lesson5

--- lesson5/exc51.py
+++ lesson5/exc51.py
@@ -1,5 +1,5 @@
 
-def is_prime():
+def is_prime(x):
     d = 2
     while x % d != 0:
         d+=1
@@ -7,7 +7,7 @@ def is_prime():
 
 x = int(input("введите число от 1 до 1000:"))
 if 1 <= x <= 1000:
-  print(is_prime())
+  print(is_prime(x))
 
 else:
     print("вы ввели неверное число")
